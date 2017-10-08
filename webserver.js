12c719cbb50f441796613ba8e355c6232d3f4657
Theron Burger <theron@theronburger.com>
2017-10-09 01:27:04 +0200

    fine.... back to 80

--- webserver.js
+++ webserver.js
@@ -10,6 +10,6 @@ var app = express();
 app.get('/*', express.static(__dirname + '/archinder.framer'));
 
 // Listen for both Heroku and local. Access locally as http://localhost:3333
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 3333);
 
 console.log("Running")
